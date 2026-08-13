02238da33c3cbf56d6e4fce94f61768469fe8856
THOTA CHANDU <chanduthota108@gmail.com>
2026-08-13 22:33:06 +0530

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int pref=1;
+        int suff=1;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+        if(pref==0) pref=1;
+        if(suff==0) suff=1;
+
+        pref*=nums[i];
+        suff*=nums[n-i-1];
+        maxi=Math.max(maxi,Math.max(pref,suff));
+        }
+        return maxi;
+    }
+}
