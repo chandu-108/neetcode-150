1c781d8490e5cca47d7401a0ae23113eef811864
THOTA CHANDU <chanduthota108@gmail.com>
2026-08-22 10:21:35 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+     if(lists==null || lists.length==0){
+        return null;
+     }
+     return func(lists,0,lists.length-1);
+    }
+    public ListNode func(ListNode[] lists,int start,int end){
+        if(start==end){
+            return lists[start];
+        }
+        if(start+1==end){
+            return merge2lists(lists[start],lists[end]);
+        }
+        int mid=start+(end-start)/2;
+        ListNode left=func(lists,start,mid);
+        ListNode right=func(lists,mid+1,end);
+        return merge2lists(left,right);
+    }
+    public ListNode merge2lists(ListNode l1,ListNode l2){
+    ListNode dummy=new ListNode(0);
+    ListNode curr=dummy;
+
+    while(l1!=null && l2!=null){
+        if(l1.val < l2.val){
+            curr.next=l1;
+            l1=l1.next;
+        }else{
+            curr.next=l2;
+            l2=l2.next;
+        }
+        curr=curr.next;
+    }
+    curr.next=(l1!=null)?l1:l2;
+    return dummy.next;
+    }
+}
